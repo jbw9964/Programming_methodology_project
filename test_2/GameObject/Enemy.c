4acd8da3c6c6be29a308ec94371223f96eb292bc
jbw9964 <jbw9964@gmail.com>
2023-12-09 22:56:53 +0900

    test: build a module about Enemy build a module about Enemy object.
    - struct Enemy_Object
    - + more functions

---
+++ test_2/GameObject/Enemy.c
@@ -0,0 +1,330 @@
+
+# ifndef __ENEMY__C
+# define __ENEMY__C
+
+# include "./Enemy.h"
+
+// temporary function to test
+void init_enemy()
+{
+    test_enemy = Create_Enemy_Object("../asset/image/enemy.png", Main_Window_Renderer, 750, 210);
+    test_enemy_2 = Create_Enemy_Object("../asset/image/enemy.png", Main_Window_Renderer, 5 * 30, 210);
+
+    SDL_Rect *src_rects[NUM_OF_ENEMY_DIRECTION];
+
+    src_rects[ENEMY_LEFT] = (SDL_Rect *) malloc(sizeof(SDL_Rect));
+    src_rects[ENEMY_LEFT]->x = 30;
+    src_rects[ENEMY_LEFT]->y = 30;
+    src_rects[ENEMY_LEFT]->w = 30;
+    src_rects[ENEMY_LEFT]->h = 30;
+
+    src_rects[ENEMY_RIGHT] = (SDL_Rect *) malloc(sizeof(SDL_Rect));
+    src_rects[ENEMY_RIGHT]->x = 60;
+    src_rects[ENEMY_RIGHT]->y = 30;
+    src_rects[ENEMY_RIGHT]->w = 30;
+    src_rects[ENEMY_RIGHT]->h = 30;
+
+    Assign_Enemy_Rect(test_enemy, src_rects);
+    Assign_Enemy_Rect(test_enemy_2, src_rects);
+
+    for (int i = 0; i < NUM_OF_ENEMY_DIRECTION; i++)
+    {
+        free(src_rects[i]);
+        src_rects[i] = NULL;
+    }
+}
+
+Enemy_Object *Create_Enemy_Object(char *path, SDL_Renderer *render, float global_x, float global_y)
+{
+    if (!render)            // invalid argument
+    {
+        // show error & abort
+        fprintf(
+            stderr, "%s%s[Error] Given renderer must be initialized.%s\n%s:%d\n", 
+            ANSI_BOLD, ANSI_RED, ANSI_RESET,
+            __FILE__, __LINE__
+        );
+        fflush(stderr);
+        return NULL;
+    }
+
+    Enemy_Object *new_enemy = NULL;
+    SDL_Texture *loaded_texture = Load_Texture(path, render, NULL, NULL);
+
+    if (!loaded_texture)        // failed to load texture
+    {
+        fprintf(
+            stderr, "%s%s[Error] Failed to create enemy object.%s\n%s:%d\n", 
+            ANSI_BOLD, ANSI_RED, ANSI_RESET,
+            __FILE__, __LINE__
+        );
+        fflush(stderr);
+    }
+    else if (!(new_enemy = (Enemy_Object *) malloc(sizeof(Enemy_Object))))     // failed to allocate memory
+    {
+        fprintf(
+            stderr, "%s%s[Error] Failed to create new enemy object, failed to allocate memory.%s\n%s:%d\n", 
+            ANSI_BOLD, ANSI_RED, ANSI_RESET,
+            __FILE__, __LINE__
+        );
+        fflush(stderr);
+
+        SDL_DestroyTexture(loaded_texture);
+    }
+    else            // succeed to allocate memory
+    {
+        new_enemy->Tex = loaded_texture;                // assign loaded texture
+        new_enemy->Current_direction = ENEMY_LEFT;      // set initial direction
+
+        // initialize `Src_rect`
+        for (int i = 0; i < NUM_OF_ENEMY_DIRECTION; i++)
+        {
+            new_enemy->Src_rect[i].x = 0;
+            new_enemy->Src_rect[i].y = 0;
+            new_enemy->Src_rect[i].w = UNIT_PIXEL;
+            new_enemy->Src_rect[i].h = UNIT_PIXEL;
+        }
+
+        // set initial position
+        new_enemy->GlobalPos_x = global_x;
+        new_enemy->GlobalPos_y = global_y;
+        new_enemy->WindowPos_x = -10;
+        new_enemy->WindowPos_y = -10;
+        
+        // set initial speed
+        new_enemy->Moving_speed = ENEMY_MAX_SPEED_X;
+
+        // enemy was created
+        new_enemy->is_dead = false;
+    }
+
+    // if it failed to create texture, `NULL` will be returned
+    return new_enemy;
+}
+
+void Assign_Enemy_Rect(Enemy_Object *enemy, SDL_Rect *src_rect_arr[NUM_OF_ENEMY_DIRECTION])
+{
+    if (!enemy || !src_rect_arr)        // invalid arguments
+    {
+        // show error & abort
+        fprintf(
+            stderr, "%s%s[Error] Given `enemy` or `src_rect_arr` hasn't initalized.%s\n%s:%d\n", 
+            ANSI_BOLD, ANSI_RED, ANSI_RESET,
+            __FILE__, __LINE__
+        );
+        fflush(stderr);
+        return;
+    }
+
+    // check whether `*src_rect_arr` is valid
+    for (int i = 0; i < NUM_OF_ENEMY_DIRECTION; i++)
+    {
+        if (!src_rect_arr[i])       // some what invalid elements, probably due to wrong memory allocation or reference of pointer
+        {
+            // show error & abbort
+            fprintf(
+                stderr, "%s%s[Error] Invalid array element in `src_rect_arr`.%s\n%s:%d\n", 
+                ANSI_BOLD, ANSI_RED, ANSI_RESET,
+                __FILE__, __LINE__
+            );
+            fflush(stderr);
+            return;
+        }
+    }
+
+    // assign `Src_rect` elements to given values
+    for (int i = 0; i < NUM_OF_ENEMY_DIRECTION; i++)
+    {
+        enemy->Src_rect[i].x = src_rect_arr[i]->x;
+        enemy->Src_rect[i].y = src_rect_arr[i]->y;
+        enemy->Src_rect[i].w = src_rect_arr[i]->w;
+        enemy->Src_rect[i].h = src_rect_arr[i]->h;
+    }
+}
+
+void Patrol_Enemy(Enemy_Object *enemy, Map *map)
+{
+    if (!enemy || !map || !map->Map_data)   {return;}       // invalid arguments
+    else if (enemy->is_dead)                {return;}       // `enemy` is dead, don't patrol
+
+    // pull out values for convenience
+    float global_x = enemy->GlobalPos_x;
+    float global_y = enemy->GlobalPos_y;
+
+    // the indexes of interest
+    int index_x = Round(global_x / map->Shrink_ratio);
+    int index_y = Round(global_y / map->Shrink_ratio);
+
+    if (                                                // calculated index is out of range to map data
+        (index_x < 0 || index_x >= map->Arr_Width)
+        || (index_y >= map->Arr_Height) 
+    )
+    {
+        // show error & abort
+        fprintf(
+            stderr, "%s%s[Error] Unexpected error occured while patrolling enemy.%s\n%s:%d\n", 
+            ANSI_BOLD, ANSI_RED, ANSI_RESET,
+            __FILE__, __LINE__
+        );
+        fflush(stderr);
+        return;
+    }
+
+/*
+//         [left block]            [Enemy]             [right block]
+//      [lower left block]      [lower block]       [lower right block]
+*/
+
+    // index of left block
+    int index_left_x = index_x - 1;
+    int index_left_y = index_y;
+
+    // index of right block
+    int index_right_x = index_x + 1;
+    int index_right_y = index_y;
+
+    // index of lower block
+    int index_lower_x = index_x;
+    int index_lower_y = index_y + 1;
+
+    // index of lower left block
+    int index_lower_left_x = index_x - 1;
+    int index_lower_left_y = index_y + 1;
+
+    // index of lower right block
+    int index_lower_right_x = index_x + 1;
+    int index_lower_right_y = index_y + 1;
+
+    // map data to it's coordinates
+    // if `data=true` there's a block in coordinate
+    int data_left = map->Map_data[index_left_y][index_left_x];
+    int data_right = map->Map_data[index_right_y][index_right_x];
+    int data_lower = map->Map_data[index_lower_y][index_lower_x];
+    int data_lower_left = map->Map_data[index_lower_left_y][index_lower_left_x];
+    int data_lower_right = map->Map_data[index_lower_right_y][index_lower_right_x];
+
+    // lower block is missing, drop enemy to ground
+    if (!data_lower)    {enemy->GlobalPos_y += ENEMY_MAX_SPEED_Y * UNIT_TIME_STEP;}
+
+    // both left & right block is missing, available to move both side
+    if (!data_left && !data_right)
+    {
+        if (data_lower_left && !data_lower_right)           // it can move left
+        {
+            enemy->Moving_speed = -1 * ENEMY_MAX_SPEED_X;   // assign speed
+            enemy->Current_direction = ENEMY_LEFT;          // set direction
+        }
+        else if (!data_lower_left && data_lower_right)      // it can move right
+        {
+            enemy->Moving_speed = ENEMY_MAX_SPEED_X;        // assign speed
+            enemy->Current_direction = ENEMY_RIGHT;         // set direction
+        }
+    }
+    else if (data_left && data_right)           // both left & right direction were blocked
+    {
+        enemy->Moving_speed = 0;                // enemy can't move any direction
+    }
+    else if (data_left)                         // only right block is missing
+    {
+        if (data_lower_right && !data_right)    // can move right
+        {
+            enemy->Moving_speed = ENEMY_MAX_SPEED_X;    // assign speed
+            enemy->Current_direction = ENEMY_RIGHT;     // set direction
+        }
+        else
+        {
+            enemy->Moving_speed = 0;            // enemy can't move any direction
+        }
+    }
+    else                                        // only left block is missing
+    {
+        if (data_lower_left && !data_left)      // can move left
+        {
+            enemy->Moving_speed = -1 * ENEMY_MAX_SPEED_X;   // assign speed
+            enemy->Current_direction = ENEMY_LEFT;          // set direction
+        }
+        else                                    // enemy can't move any direction
+        {
+            enemy->Moving_speed = 0;
+        }
+    }
+
+    // move enemy with it's speed
+    enemy->GlobalPos_x += enemy->Moving_speed * UNIT_TIME_STEP;
+}
+
+void Apply_Enemy_Player_physics(Player *character, Enemy_Object *enemy)
+{
+    if (!character || !enemy)        {return;}      // invalid arguments
+    else if (enemy->is_dead)        {return;}       // enemy is dead, don't apply physics
+
+    // coordinate of character
+    SDL_FRect char_rect = {
+        character->GlobalPos_x,
+        character->GlobalPos_y,
+        UNIT_PIXEL, UNIT_PIXEL
+    };
+
+    // coordinate of enemy
+    SDL_FRect enemy_rect = {
+        enemy->GlobalPos_x,
+        enemy->GlobalPos_y,
+        UNIT_PIXEL, UNIT_PIXEL
+    };
+
+    if (SDL_HasIntersectionF(&char_rect, &enemy_rect))      // if character & enemy were encountered,
+    {
+        if (enemy->GlobalPos_y - character->GlobalPos_y > UNIT_PIXEL / 2)   // and if character stepped on enemy,
+        {
+            enemy->is_dead = true;                              // kill enemy
+            character->Speed_y = MAX_SPEED_Y / 2;               // bounce character
+        }
+        else            // character crashed with enemy
+        {
+            character->is_dead = true;  // kill player
+        }
+    }
+}
+
+void Render_Enemy(Enemy_Object *enemy, SDL_Renderer *render, float character_global_x, float character_window_x)
+{
+    if (!enemy || !render)      {return;}       // invalid arguments
+
+    // pull out values for convenience
+    float enemy_global_x = enemy->GlobalPos_x;
+    float enemy_global_y = enemy->GlobalPos_y;
+
+    // establish the window coordinate to render `enemy`
+    enemy->WindowPos_x = enemy_global_x - character_global_x + character_window_x;
+    enemy->WindowPos_y = enemy_global_y;
+
+    // the window coordinate is beyond current window, don't render
+    if (enemy->WindowPos_x > WIN_WIDTH || enemy->WindowPos_x < 0)   {return;}
+    // enemy is dead, don't render
+    else if (enemy->is_dead)                                        {return;}   
+
+    // the window coordinate to render character
+    SDL_FRect dst_rect = {
+        enemy->WindowPos_x + UNIT_PIXEL / 2,
+        enemy->WindowPos_y + UNIT_PIXEL / 2,
+        UNIT_PIXEL, UNIT_PIXEL
+    };
+
+    Enemy_Direction direction = enemy->Current_direction;
+
+    // render enemy texture with it's current direction to window coordinate
+    SDL_RenderCopyF(render, enemy->Tex, &enemy->Src_rect[direction], &dst_rect);
+}
+
+void dispose_enemy(Enemy_Object *enemy)
+{
+    if (enemy)          // good to dispose
+    {
+        // good to dispose
+        if (enemy->Tex)     {SDL_DestroyTexture(enemy->Tex);}   // free texture
+        enemy->Tex = NULL;
+        free(enemy);        // free allocated memory
+    }
+}
+
+# endif
---
+++ test_2/GameObject/Enemy.h
@@ -0,0 +1,100 @@
+
+# ifndef __ENEMY__H
+# define __ENEMY__H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+# include <SDL2/SDL.h>
+# include <SDL2/SDL_image.h>
+
+# include "../def.h"
+# include "./Player.h"
+# include "../Init/Map.h"
+
+
+// @brief   A enumerate that represent the moving direction of enemy
+typedef enum Enemy_Direction{
+    ENEMY_LEFT,
+    ENEMY_RIGHT
+} Enemy_Direction;
+
+/**
+ * @brief   A structure that represent the enemy.
+ *  
+ * @param   `Tex`               the texture to render enemy
+ * @param   `Src_rect[]`        the array of `SDL_Rect *`, each `rect` represent the position of `Tex` depends on `Current_direction`
+ * @param   `Current_direction` the moving direction of enemy (`ENEMY_LEFT`, `ENEMY_RIGHT`)
+ *  
+ * @param   `GlobalPos_~`       the global position of enemy (`MAP`)
+ * @param   `WindowPos_~`       the relative position of enemy with window (`WINDOW`, `RELATIVE`)
+ *  
+ * @param   `Moving_speed`      the moving speed of enemy
+ * @param   `is_dead`           represent enemy is dead or not
+ *  
+ * @details The `GlobalPos_x` will be calculated by `+= Moving_speed * UNIT_TIME_STEP` in `Patrol_Enemy` function.
+ */
+typedef struct Enemy_Object
+{
+    SDL_Texture *Tex;
+    SDL_Rect Src_rect[NUM_OF_ENEMY_DIRECTION];
+    Enemy_Direction Current_direction;
+    
+    float GlobalPos_x;
+    float GlobalPos_y;
+    float WindowPos_x;
+    float WindowPos_y;
+
+    float Moving_speed;
+    bool is_dead;
+} Enemy_Object;
+
+
+/**
+ * @brief   Create `Enemy_Object` structure with given arguments.
+ * 
+ * @param   `path`              path to load PNG image 
+ * @param   `render`            `SDL_Renderer *` to render image
+ * @param   `global_~`          the global coordinate to spawn enemy
+ *  
+ * @return  `Enemy_Object *`    :   allocated memory
+ */
+Enemy_Object *Create_Enemy_Object(char *path, SDL_Renderer *render, float global_x, float global_y);
+
+// @brief   Assign `enemy->Src_rect[]` with each `Enemy_Direction`.
+void Assign_Enemy_Rect(Enemy_Object *enemy, SDL_Rect *src_rect_arr[NUM_OF_ENEMY_DIRECTION]);
+
+/**
+ * @brief   Move `enemy` left or rigth with it's coordinate.
+ *  
+ * @param   `enemy`     `Enemy_Object *` to patrol
+ * @param   `map`       `Map *` to read map data 
+ *  
+ * @details `enemy` will patrol left or right. If `enemy` encounter obstacle or clif, it will change direction and keep moving.
+ * And if `enemy` is dead (`is_dead=true`), it will do nothing.
+ * @warning If `enemy` or `map` or `map->Map_data` wasn't initialized, it will do nothing.
+ */
+void Patrol_Enemy(Enemy_Object *enemy, Map *map);
+
+/**
+ * @brief   Apply `Enemy & character physics`.
+ * 
+ * @details If `character` steps on `enemy`, it will kill enemy & bouns a bit. Else if `character` encounters `enemy`, it will kill `character`.
+ * Kill means `is_dead=true`.
+ */
+void Apply_Enemy_Player_physics(Player *character, Enemy_Object *enemy);
+
+// @brief   Render `enemy` to given arguments. It acquire chracter's `global position` & `window position`.
+void Render_Enemy(Enemy_Object *enemy, SDL_Renderer *render, float character_global_x, float character_window_x);
+
+// @brief   Free allocated memory assigned to `enemy`.
+void dispose_enemy(Enemy_Object *enemy);
+
+extern SDL_Texture *Load_Texture(char *path, SDL_Renderer *render, int *w, int *h);
+
+Enemy_Object *test_enemy;
+Enemy_Object *test_enemy_2;
+extern SDL_Renderer *Main_Window_Renderer;
+
+# endif
